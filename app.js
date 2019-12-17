ca0b34a6dc903edbb93bf719b023ec07ca8c3514
Marcos Guedes <mguedes@growin.pt>
2019-12-17 15:32:55 +0000

    pug example finished

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', './src/views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'My Library', list: ['a', 'b'] });
 });
 
 app.listen(port, () => {

207d982b3a075087b1ea7628d3ff89babc5112b5
Marcos Guedes <mguedes@growin.pt>
2019-12-20 14:07:28 +0000

    create user working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +14,10 @@ app.use(morgan('tiny'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport')(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -4,11 +4,40 @@ const debug = require('debug')('app:authRoutes');
 
 function router() {
   const authRouter = express.Router();
+  const url = 'mongodb://localhost:27017';
+  const dbName = 'DbLibrary';
 
   authRouter.route('/signUp')
     .post((req, res) => {
-      debug(req.body);
-      res.json(req.body);
+      const { username, password } = req.body;
+
+      (async () => {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+          const col = await db.collection('users');
+          const user = { username, password };
+
+          const results = await col.insertOne(user);
+          debug(results);
+
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (err) {
+          debug(err);
+        } finally {
+          client.close();
+        }
+      })();
+    });
+
+  authRouter.route('/profile')
+    .get((req, res) => {
+      res.json(req.user);
     });
 
   return authRouter;

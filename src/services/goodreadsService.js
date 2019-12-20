3c5b562c308a1158a5bfe6b78c6edc6fc728da8f
Marcos Guedes <mguedes@growin.pt>
2019-12-20 16:26:11 +0000

    call service with axios working

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,7 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
+
 function goodreadsService() {
-  function getBookById() {
-    return new Promise((resolve) => {
-      resolve({ description: 'our description' });
+  function getBookById(id) {
+    return new Promise((resolve, reject) => {
+      axios.get(`https://www.goodreads.com/book/show/${id}.xml?key=iHHO0UxlAuH62CO5Lq1Lg`)
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
 
